ccdfff94d6ef60f9fb6178a7f020231855141b09
mattaxell <hello@mattaxell.com>
2017-12-07 18:00:36 +0000

    Add blank home page template

---
+++ views/home.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Template Name: Home
+ * ------------------------------ *
+ * Template used for custom, static home page
+ */
+get_header();
+the_post();
+the_content();
+get_footer();
