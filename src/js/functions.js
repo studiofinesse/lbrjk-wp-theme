7557e030b7a2e109294052a8b9f4f2da981a1512
mattaxell <hello@mattaxell.com>
2020-02-13 19:27:28 +0000

    Remove use of aos.js

--- src/js/functions.js
+++ src/js/functions.js
@@ -1,14 +1,9 @@
 (function($) {
 
-	// Initiate AOS
-	// AOS.init({
-	// 	once: true
-	// });
-
 	$(document).ready(function() {
 
 		// Mobile menu trigger
-		$('.toggle--menu').click(function(){
+		$('.toggle-menu').click(function(){
 			$(this).toggleClass('active');
 			$('html').toggleClass('nav-open');
 		});
